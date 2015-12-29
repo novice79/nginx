f88a3f57d77f44547cecbe361654376c9c337627
david <david@cninone.com>
2015-12-29 14:20:31 +0800

    add nginx reverse proxy docker image

---
+++ nginx/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="utf-8" /> 
+<title>nginx in docker</title> 
+<style> 
+	body{ text-align:center} 
+	.div{ margin:0 auto; width:600px; height:20px; border:1px solid #F00} 
+</style> 
+</head> 
+<body> 
+	<div class="div"> 
+		nginx in docker
+	</div> 
+
+</body> 
+</html> 
+
+
+
