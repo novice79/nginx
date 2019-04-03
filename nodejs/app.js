17a544b55e958716aa8f427268a6fe0290354d08
novice <david@cninone.com>
2019-04-03 16:39:02 +0800

    add cronjob for cert renew

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,6 +8,7 @@ const io = require('socket.io')(server);
 const bodyParser = require('body-parser')
 const session = require('express-session');
 const nunjucks = require('nunjucks');
+const CronJob = require('cron').CronJob;
 const util = require('./util');
 const record = "/etc/letsencrypt/cert2svr.json";
 let cert2svr = {};
@@ -20,6 +21,13 @@ if (fs.existsSync(record)) {
     }
   }
 }
+// 1st of every month at 3 am
+const job = new CronJob('0 0 3 1 * *', () => {
+  const exe_log = execSync('letsencrypt renew').toString().trim();
+  console.log(exe_log);
+  execSync('service nginx reload');
+}, null, false, 'Asia/Shanghai');
+job.start();
 nunjucks.configure(__dirname + '/views', { autoescape: true });
 app.use(require('express').static(__dirname + '/public'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
